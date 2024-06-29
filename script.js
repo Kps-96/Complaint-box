dad24f1eda8d0b43147ace89dd46febad2654f15
Kps-96 <kpsitmail007@gmail.com>
2024-06-29 17:14:08 +0530

    almost done

--- script.js
+++ script.js
@@ -2,7 +2,13 @@ function check(){
     const mail = document.getElementById("email");
     let mailValue = mail.value;
     let emailPattern = /^[0-9._%+-]+@srcas\.ac\.in$/;
+    let sen = document.getElementById("complaint-area");
+    let val = sen.value;
     if (!emailPattern.test(mailValue)) {
         alert('Please enter a valid email address ending with @srcas.ac.in');
+    }
+    
+    if ("" === val) {
+        alert("Text area empty ! Please enter your words")
     }
 }
