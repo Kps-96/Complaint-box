7d442f84f4f201393d24877aff966b754ce5b708
Kps-96 <kpsitmail007@gmail.com>
2024-06-29 21:29:15 +0530

    almost completed

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
-function check(){
+function check()
+{
     const mail = document.getElementById("email");
     let mailValue = mail.value;
     let emailPattern = /^[0-9._%+-]+@srcas\.ac\.in$/;
     let sen = document.getElementById("complaint-area");
     let val = sen.value;
-    if (!emailPattern.test(mailValue)) {
+    if (!emailPattern.test(mailValue)) 
+    {
         alert('Please enter a valid email address ending with @srcas.ac.in');
-    }
-    
-    if ("" === val) {
-        alert("Text area empty ! Please enter your words")
+        // let spn = document.getElementById("span");
+        // spn.style.display = "block";
     }
 
-//     success();
-
-//     setInterval(function success(){
-//         let img = document.getElementById("submitted-img");
-//         img.style.display = "block";
-//     }, 2000);
+    else if ("" === val) 
+    {
+        alert("Text area empty ! Please enter your words");
+    }
 }
 
+    //   setInterval(function success()
+    //     {
+    //     let img = document.getElementById("submitted-img");
+    //     img.style.display = "block";
+    // }, 2000);
 
+    
+    
+
+
+
+
+  
